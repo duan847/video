c762ad2fd6a63956d7837c0c1396c4ba6a326ddf
段佳维 <554343346@qq.com>
2019-06-27 19:26:31 +0800

    爬取地址切换到80

--- scp-video.sh
+++ scp-video.sh
@@ -1,1 +1,1 @@
-mvn clean && mvn package -Dmaven.test.skip=true && scp target/video-*.jar root@129.204.29.217:/root
+mvn clean && mvn package -Dmaven.test.skip=true && scp target/video-*.jar root@39.106.164.245:/root
